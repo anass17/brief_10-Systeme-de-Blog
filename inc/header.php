d60e52bf81f2115280a5e983ad6f9746713ff422
anass17 <anassboutaib2018@gmail.com>
2025-01-05 15:23:49 +0100

    Added file that will be included in pages

---
+++ inc/header.php
@@ -0,0 +1,13 @@
+<div class="bg-gradient-to-b from-orange-500 to-orange-600">
+    <div class="max-w-[1250px] mx-auto px-3 h-16 flex justify-between items-center">
+        <div>
+            <a href="#" class="font-semibold text-lg no-underline text-white">WisNest</a>
+        </div>
+        <div class="*:ml-6 *:font-semibold text-white">
+            <a href="/index.php">Home</a>
+            <a href="/pages/blogs.php">Blogs</a>
+            <a href="/pages/auth.php">Login</a>
+            <a href="/pages/auth.php?to=register">Register</a>
+        </div>
+    </div>
+</div>
