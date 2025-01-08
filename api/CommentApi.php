6c016667eef49515334c7022267b4f86aeb3f756
anass17 <anassboutaib2018@gmail.com>
2025-01-08 12:44:15 +0100

    Added Comment API

---
+++ api/CommentApi.php
@@ -0,0 +1,120 @@
+<?php
+
+    session_start();
+
+    require '../Controllers/Classes/Database.php';
+    require '../Controllers/Classes/User.php';
+    require '../Controllers/Classes/Auth.php';
+    require '../Controllers/Classes/Post.php';
+    require '../Controllers/Classes/Comment.php';
+    require '../Controllers/Classes/Helpers.php';
+
+    header('Content-Type: application/json');
+
+    if (isset($_SERVER["QUERY_STRING"])) {
+
+    }
+
+    $db = new Database();
+    $auth = new Auth($db);
+    $helpers = new Helpers();
+    $post = new Post($db);
+    $comment = new Comment($db);
+
+    // Check if access token does not exist
+
+    if (!$auth -> isAccessTokenExists()) {
+        echo json_encode([
+            'result' => false,
+            'error' => 'You are not authorized to perform this action'
+        ]);
+        exit;
+    }
+
+    $get_payload_date = file_get_contents('php://input');
+    $data = json_decode($get_payload_date);
+
+    switch ($_SERVER["REQUEST_METHOD"]) {
+
+        // Create Comment
+
+        case 'POST':
+            
+            $post_id = isset($data -> postId) ? $data -> postId : '';
+            $post_content = isset($data -> commentContent) ? $data -> commentContent : '';
+
+            $post -> setId($post_id);
+
+            if (!$post -> createComment($post_content, $auth -> user -> getId())) {
+                echo json_encode([
+                    'result' => false,
+                    'error' => 'Could not process your request'
+                ]);
+            } else {
+                echo json_encode([
+                    'result' => true,
+                    'error' => '',
+                    'details' => [
+                        'firstName' => $auth -> user -> getFirstName(),
+                        'lastName' => $auth -> user -> getLastName(),
+                        'content' => $helpers -> format_text($post_content)
+                    ]
+                ]);
+            }
+
+            break;
+        
+        // Update comment
+
+        case 'PUT':
+
+            $comment_id = isset($data -> commentId) ? $data -> commentId : '';
+            $comment_content = isset($data -> commentContent) ? $data -> commentContent : '';
+    
+            $comment -> setId($comment_id);
+            $comment -> setContent($comment_content);
+    
+            if (!$comment -> updateComment()) {
+                echo json_encode([
+                    'result' => false,
+                    'error' => $comment -> getErrors()[0]
+                ]);
+            } else {
+                echo json_encode([
+                    'result' => true,
+                    'error' => '',
+                    'content' => $helpers -> format_text($comment_content)
+                ]);
+            }
+
+            break;
+
+        // Delete comment
+
+        case 'DELETE':
+
+            $comment_id = isset($data -> commentId) ? $data -> commentId : '';
+
+            $comment -> setId($comment_id);
+
+            if (!$comment -> deleteComment()) {
+                echo json_encode([
+                    'result' => false,
+                    'error' => $comment -> getErrors()[0]
+                ]);
+            } else {
+                echo json_encode([
+                    'result' => true,
+                    'error' => ''
+                ]);
+            }
+
+            break;
+        
+        default:
+            echo json_encode([
+                'result' => false,
+                'error' => 'Invalid Request!'
+            ]);
+    }
+    
