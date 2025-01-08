fc556096b989725f3ae56d9eaecfa851dd45e923
anass17 <anassboutaib2018@gmail.com>
2025-01-08 12:45:41 +0100

    Updated

--- assets/js/blogs.js
+++ assets/js/blogs.js
@@ -6,7 +6,7 @@ writeBlogBtn.addEventListener('click', function () {
     modal.className = "bg-black bg-opacity-70 backdrop-blur-sm fixed top-0 left-0 z-50 flex justify-center items-center w-full h-screen";
 
     modal.innerHTML = 
-    `<div class="bg-white w-full max-w-xl rounded max-h-[650px] overflow-auto shadow-lg">
+    `<div class="bg-white w-full max-w-xl rounded max-h-[650px] overflow-auto shadow-lg no-scrolling">
             
         <div class="px-7 py-4 flex justify-between items-center border-b border-gray-300">
             <h2 class="text-lg font-bold text-gray-700">Write a New Blog</h2>
@@ -30,19 +30,29 @@ writeBlogBtn.addEventListener('click', function () {
                 </div>
 
                 <div class="mb-4">
-                    <label for="form-post-category" class="block mb-2">category</label>
-                    <select id="form-post-category" name="post-category" class="w-full px-3 py-2 rounded border outline-none border-orange-200 bg-gray-100 placeholder:text-gray-500">
-                        <option value="">SELECT</option>
-                        <option value="1">Web Development</option>
-                        <option value="2">Artificial Inteligence</option>
-                        <option value="3">Problem Solving</option>
-                    </select>
+                    <div class="flex items-center justify-between mb-2">
+                        <label for="form-post-tags" class="block mb-2">Tags <span class="ml-5 text-gray-500">[<span class="tags-count">0</span>]</span></label>
+                        <button type="button" class="tags-toggle transition-all">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 320 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+                        </button>
+                    </div>
+                    <div class="overflow-hidden tags-container transition-all" style="height: 0px;">
+                        <div class="*:text-sm grid grid-cols-3 gap-3 *:rounded *:border *:shadow *:py-1.5 pb-4">
+                            <button type="button" class="bg-gray-100 tag-btn">HTML</button>
+                            <button type="button" class="bg-gray-100 tag-btn">CSS</button>
+                            <button type="button" class="bg-gray-100 tag-btn">JS</button>
+                            <button type="button" class="bg-gray-100 tag-btn">Web Development</button>
+                            <button type="button" class="bg-gray-100 tag-btn">Front-end</button>
+                            <button type="button" class="bg-gray-100 tag-btn">Full-Stack</button>
+                        </div>
+                    </div>
+                    <input type="text" id="form-post-tags" name="post-tags" class="w-full px-3 py-2 rounded border outline-none border-orange-200 bg-gray-100 placeholder:text-gray-500">
                     <small class="text-red-400 font-semibold"></small>
                 </div>
                 
                 <div class="mb-4">
                     <p class="mb-2">Background</p>
-                    <label for="form-post-background" class="mb-2 h-16 bg-gray-500 text-white rounded flex gap-6 text-lg justify-center items-center cursor-pointer">
+                    <label for="form-post-background" class="file-label mb-2 h-16 bg-gray-500 text-white rounded flex gap-6 text-lg justify-center items-center cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" class="fill-white w-6 h-6" viewBox="0 0 640 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128l-368 0zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39L296 392c0 13.3 10.7 24 24 24s24-10.7 24-24l0-134.1 39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"/></svg>    
                         Upload an Image
                     </label>
@@ -61,10 +71,11 @@ writeBlogBtn.addEventListener('click', function () {
         modal.remove();
     });
 
+    let postTitle = modal.querySelector('#form-post-title');
+    let postContent = modal.querySelector('#form-post-content');
+    let postTags = modal.querySelector('#form-post-tags');
+
     modal.querySelector('.submit-btn').addEventListener('click', function (e) {
-        let postTitle = modal.querySelector('#form-post-title');
-        let postContent = modal.querySelector('#form-post-content');
-        let postCategory = modal.querySelector('#form-post-category');
         let hasErrors = false;
 
         if (postTitle.value.trim().length < 5) {
@@ -81,17 +92,48 @@ writeBlogBtn.addEventListener('click', function () {
             postContent.nextElementSibling.textContent = "";
         }
 
-        if (postCategory.value == "") {
-            postCategory.nextElementSibling.textContent = "Please select a category";
+        if (postTags.value == "") {
+            postTags.nextElementSibling.textContent = "Please select at least a tag";
             hasErrors = true;
         } else {
-            postCategory.nextElementSibling.textContent = "";
+            postTags.nextElementSibling.textContent = "";
         }
 
         if (hasErrors == true) {
             e.preventDefault();
         }
         
+    });
+
+    document.querySelector('#form-post-background').addEventListener('input', function () {
+        this.previousElementSibling.innerHTML = "Successfully Uploaded";
+    });
+
+    modal.querySelector('.tags-toggle').addEventListener('click', function () {
+        let tagsContainer = modal.querySelector('.tags-container');
+        if (tagsContainer.style.height == "0px") {
+            tagsContainer.style.height = "100px";
+            this.style.transform = "rotate(90deg)";
+        } else {
+            tagsContainer.style.height = "0px";
+            this.style.transform = "rotate(0deg)";
+        }
+    });
+
+    let tagsCount = modal.querySelector('.tags-count');
+
+    modal.querySelectorAll('.tag-btn').forEach((btn) => {
+        btn.addEventListener('click', function () {
+            if (this.classList.contains('selected')) {
+                this.classList.remove('selected');
+                tagsCount.textContent = tagsCount.textContent - 1;
+                postTags.value = postTags.value.replace(this.textContent + ',', '');
+            } else {
+                this.classList.add('selected');
+                tagsCount.textContent = +tagsCount.textContent + 1;
+                postTags.value += this.textContent + ',';
+            }
+        });
     });
 
 });
